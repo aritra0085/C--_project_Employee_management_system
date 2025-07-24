ebbf48af5c120cd9efba746bb59e915991363780
aritra0085 <aritrabiswas0085@gmail.com>
2025-07-24 23:51:56 +0530

    write the else part

--- employee_management_system.cpp
+++ employee_management_system.cpp
@@ -85,3 +85,75 @@ void enter()
             cin >> email[i];
         }
     }
+
+    else
+
+    {
+
+        for (int i = total; i < ch + total; i++) // to store onward students' data rather than storing double data
+
+        {
+
+            cout << "\nEnter the Data of Employee " << i + 1 << endl
+                 << endl;
+            cout << "** PERSONAL DETAILS **" << endl
+                 << endl;
+
+            cout << "Enter Name: " << endl;
+
+            cin >> name[i];
+
+            cout << "Enter Address: " << endl;
+
+            cin >> address[i];
+
+            cout << "Enter Date of Birth: " << endl;
+
+            cin >> dob[i];
+
+            cout << "Is Employee Married?: " << endl;
+
+            cin >> marstatus[i];
+
+            cout << "** WORK DETAILS **" << endl
+                 << endl;
+
+            cout << "Enter Id: " << endl;
+
+            cin >> id[i];
+
+            cout << "Enter Date of Joing: " << endl;
+
+            cin >> doj[i];
+
+            cout << "Work Department: " << endl;
+
+            cin >> workd[i];
+
+            cout << "Enter Work Location: " << endl;
+
+            cin >> workl[i];
+
+            cout << "Enter CTC: " << endl;
+
+            cin >> ctc[i];
+
+            cout << "Enter Social Insurance Id: " << endl;
+
+            cin >> socialins[i];
+
+            cout << "** CONTACT DETAILS **" << endl
+                 << endl;
+
+            cout << "Enter Mobile NO: " << endl;
+
+            cin >> mobile_no[i];
+
+            cout << "Enter Email: " << endl;
+
+            cin >> email[i];
+        }
+
+        total = ch + total; // making choice
+    }
+}
